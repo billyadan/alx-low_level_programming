b2d83fded7f9e40a4dd0a08c1a67ad4e26ed32b7
Billy Adan <mr.billyadan@gmail.com>
2023-08-31 15:34:05 -0700

    Mandatory Tasks

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,7 +1,15 @@
 #include <unistd.h>
 #include "main.h"
 
+/*
+// _putchar - writes char c to stdout
+// @c: char to print
+//
+// Return: On success 1.
+// On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
